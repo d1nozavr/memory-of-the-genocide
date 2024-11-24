b5e539f7df11949f3adfeb158fcc08d2783e8cdc
Yaroslav <trifonovjroslav2017@mail.ru>
2024-11-24 10:08:46 +0300

    version 0.94-0.95

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,44 @@
-function toggleMenu() {
-    document.getElementById('sidebar').classList.toggle('active');
+document.querySelectorAll('.topic-header').forEach(header => {
+    header.addEventListener('click', () => {
+        const content = header.nextElementSibling;
+
+        content.style.display = content.style.display === 'block' ? 'none' : 'block';
+
+        if (content.style.display === 'block') {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+
+        else {
+            content.style.maxHeight = null;
+        }
+    });
+});
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide) => {
+        slide.style.display = 'none';
+    });
+
+    if (index >= slides.length) {
+        currentSlide = 0;
+    }
+
+    else if (index < 0) {
+        currentSlide = slides.length - 1;
+    }
+
+    else {
+        currentSlide = index;
+    }
+
+    slides[currentSlide].style.display = 'block';
 }
+
+function changeSlide(n) {
+    showSlide(currentSlide + n);
+}
+
+showSlide(currentSlide);
